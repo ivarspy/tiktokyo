1c8735c5c8aa530251f8348280611e1199a59fc2
Peter <19887796+ivarspy@users.noreply.github.com>
2026-02-13 14:13:14 -0500

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -39,7 +39,7 @@ export default buildConfig({
   db: sqliteD1Adapter({ binding: cloudflare.env.D1 }),
   plugins: [
     r2Storage({
-      bucket: cloudflare.env.R2,
+      bucket: (cloudflare.env.R2 as unknown) as any,
       collections: { media: true },
     }),
   ],
